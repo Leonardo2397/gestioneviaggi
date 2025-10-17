bdd8813496dc5d627ef2477db6e10ec96e73bb79
Leonardo2397 <leonardoferrante753@gmail.com>
2025-10-17 10:35:45 +0200

    Initial commit

---
+++ gestioneviaggi/src/test/java/leonardoferrante/gestioneviaggi/GestioneviaggiApplicationTests.java
@@ -0,0 +1,13 @@
+package leonardoferrante.gestioneviaggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneviaggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
